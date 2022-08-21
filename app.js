eedd5423235466056d5070f6bada997920543b39
jeroenbnn <Jeroenboonen@gmail.com>
2022-08-21 11:16:02 +0200

    update kersttv.be

--- app.js
+++ app.js
@@ -1,40 +1,113 @@
 const APPLICATION_ID = "A25F6603";
 const NAMESPACE_URN = "urn:x-cast:chromecast-office-dashboard.firebaseapp.com";
 
-window['__onGCastApiAvailable'] = function(loaded, errorInfo) {
-  if (!loaded) {
-    console.log(errorInfo);
-  }
+window['__onGCastApiAvailable'] = function (loaded, errorInfo) {
+    if (!loaded) {
+        console.log(errorInfo);
+    }
 };
 
-$( document ).ready(function() {
-  $("#open").click(function(){
-    open();
+$(document).ready(function () {
+    $("#open").click(function () {
+        open();
+    });
+});
+
+open = function () {
+    var context = cast.framework.CastContext.getInstance();
+
+    context.setOptions({
+        receiverApplicationId: "A25F6603",
+        autoJoinPolicy: chrome.cast.AutoJoinPolicy.TAB_AND_ORIGIN_SCOPED,
+        resumeSavedSession: true,
+    });
+
+    context.requestSession().then(function (e) {
+        var url = $("#url").val();
+        var s = context.getCurrentSession();
+        s.setVolume(80).then(function () {
+            console.log(url);
+            return s.sendMessage(NAMESPACE_URN, {
+                "url": url
+            });
+        }).then(function () {
+            context.endCurrentSession(false);
+        }).catch(function (e) {
+            console.log(e);
+        })
+
+    }).catch(function (e) {
+        console.log(e);
+    })
+};
+
+
+
+$(document).ready(function () {
+    $("#open2").click(function () {
+        open2();
+    });
+});
+
+open2 = function () {
+    var context = cast.framework.CastContext.getInstance();
+
+    context.setOptions({
+        receiverApplicationId: "A25F6603",
+        autoJoinPolicy: chrome.cast.AutoJoinPolicy.TAB_AND_ORIGIN_SCOPED,
+        resumeSavedSession: true,
+    });
+
+    context.requestSession().then(function (e) {
+        var url = $("#url2").val();
+        var s = context.getCurrentSession();
+        s.setVolume(80).then(function () {
+            console.log(url);
+            return s.sendMessage(NAMESPACE_URN, {
+                "url": url
+            });
+        }).then(function () {
+            context.endCurrentSession(false);
+        }).catch(function (e) {
+            console.log(e);
+        })
+
+    }).catch(function (e) {
+        console.log(e);
+    })
+};
+
+
+$(document).ready(function () {
+  $("#open3").click(function () {
+      open2();
   });
 });
 
-open = function() {
+open2 = function () {
   var context = cast.framework.CastContext.getInstance();
 
   context.setOptions({
-    receiverApplicationId: "A25F6603",
-    autoJoinPolicy: chrome.cast.AutoJoinPolicy.TAB_AND_ORIGIN_SCOPED,
-    resumeSavedSession: true,
+      receiverApplicationId: "A25F6603",
+      autoJoinPolicy: chrome.cast.AutoJoinPolicy.TAB_AND_ORIGIN_SCOPED,
+      resumeSavedSession: true,
   });
 
-  context.requestSession().then(function(e){
-    var url = $("#url").val();
-    var s = context.getCurrentSession();
-    s.setVolume(0).then(function(){
-      console.log(url);
-      return s.sendMessage(NAMESPACE_URN, {"url": url});
-    }).then(function(){
-      context.endCurrentSession(false);
-    }).catch(function(e){
-      console.log(e);
-    })
+  context.requestSession().then(function (e) {
+      var url = $("#url3").val();
+      var s = context.getCurrentSession();
+      s.setVolume(80).then(function () {
+          console.log(url);
+          return s.sendMessage(NAMESPACE_URN, {
+              "url": url
+          });
+      }).then(function () {
+          context.endCurrentSession(false);
+      }).catch(function (e) {
+          console.log(e);
+      })
 
-  }).catch(function(e){
-    console.log(e);
+  }).catch(function (e) {
+      console.log(e);
   })
 };
